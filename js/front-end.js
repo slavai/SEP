e4d30b6a76cfdcbb64a966268b153cb94ed8ebe4
Slava <isaevslava87@gmail.com>
2013-12-31 17:01:22 +0200

    new styles

--- js/front-end.js
+++ js/front-end.js
@@ -9,6 +9,19 @@ jQuery(document).ready(function(){
         enableTouch: false
     });
 
+//    modal
+    jQuery(".responder_button").click(function(e){
+        var CuponId = jQuery(this).parent().attr('data-cupon-id');
+        jQuery('body').addClass('modalOpen').append('<div class="overlay"></div>');
+        jQuery('.modal').show();
+
+        jQuery('.overlay').click(function(e){
+            colapsF();
+            return false
+        });
+        return false
+    });
+
 
 //    var parser = new UAParser();
 //    alert(parser.getResult().os.name);
@@ -61,6 +74,7 @@ jQuery(document).ready(function(){
     })
 
     jQuery('.remove_fixed_footer_button').click(function(e){
+        jQuery('#content').css('padding-bottom', '15px')
         jQuery(this).parent('footer').removeClass('fixed_footer');
         jQuery(this).hide();
         return false;
@@ -85,4 +99,39 @@ jQuery(document).ready(function(){
     jQuery('.hearts_count').click(function(e){
        jQuery(this).toggleClass('active');
     });
+
+    if(jQuery('.fixed_footer').size() > 0){
+        var footerHeight = jQuery('.fixed_footer').height();
+        jQuery('#content').css('padding-bottom', footerHeight + 15 + 'px')
+    }
+    jQuery('.infoButton.infoButtonHide').click(function(){
+        jQuery(this).parents('.text_block').hide();
+        jQuery('.infoButtonShow').show();
+    });
+
+    jQuery('.infoButton.infoButtonShow').click(function(){
+        jQuery(this).prev('.text_block').show();
+        jQuery(this).hide();
+    });
+
+    // cupon slide up/down for blocks
+    jQuery(".green_button_block").click(function(e){
+        jQuery(this).toggleClass("open");
+    });
+
+
+
 });
+
+//event on menu opens
+function OnOpenLeftNaw(){};
+function OnCloseLeftNaw(){};
+function OnOpenRightNaw(){};
+function OnCloseRightNaw(){};
+
+
+function colapsF(){
+    jQuery('body').removeClass('modalOpen');
+    jQuery('.overlay').remove();
+    jQuery('.modal').hide();
+}
